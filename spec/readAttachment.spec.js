aed4695147c371555a0184501a364350cfa5c3f3
Robb Winkle <robb@blendededge.com>
2022-05-04 19:15:57 -0400

    Small changes to component

--- spec/readAttachment.spec.js
+++ spec/readAttachment.spec.js
@@ -1,12 +1,12 @@
 const { expect } = require('chai');
-const process = require('../lib/actions/yourAction');
+const process = require('../lib/actions/readAttachment');
 
 const msg = {
   body: {},
 };
 const cfg = {};
 
-describe('Tests for action', () => {
+xdescribe('Tests for action', () => {
   it('should do action things correctly', async () => {
     const result = await process.call(msg, cfg);
     expect(result).to.be.equal(undefined);
